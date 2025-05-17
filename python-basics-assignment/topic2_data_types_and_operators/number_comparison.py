872e3d287e0e03e20539f412e6cbdecb9325e43a
Angel <166138746+aansheeagrwal@users.noreply.github.com>
2025-05-17 13:45:52 +0530

    Update number_comparison.py

--- python-basics-assignment/topic2_data_types_and_operators/number_comparison.py
+++ python-basics-assignment/topic2_data_types_and_operators/number_comparison.py
@@ -1,1 +1,13 @@
 
+# Program to compare two numbers entered by the user
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+if a > b:
+    print("The first number is greater than the second.")
+elif a < b:
+    print("The first number is less than the second.")
+else:
+    print("Both numbers are equal.")
+
