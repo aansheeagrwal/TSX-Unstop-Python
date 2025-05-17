e226e461c9549b9d3fb1ad8e2a6ef6fddb6c4bae
Angel <166138746+aansheeagrwal@users.noreply.github.com>
2025-05-17 13:46:15 +0530

    Update leap_year_check.py

--- python-basics-assignment/topic2_data_types_and_operators/leap_year_check.py
+++ python-basics-assignment/topic2_data_types_and_operators/leap_year_check.py
@@ -1,8 +1,11 @@
 
+# Program to check whether a year is a leap year
 
 year = int(input("Enter a year: "))
 
+# Leap year condition
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("Leap Year")
+    print(year, "is a Leap Year")
 else:
-    print("Not a Leap Year")
+    print(year, "is Not a Leap Year")
+
